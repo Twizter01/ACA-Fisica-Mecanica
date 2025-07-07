8ba36c7903e4136f24de97769cc3c286e8cb0ee7
Gabriel Rodriguez <gabrielandrejimenez11@gmail.com>
2025-07-07 01:41:40 +0000

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { HashRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
